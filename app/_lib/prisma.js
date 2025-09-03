acad91be8a6a227583941f96652ed23688ca0105
TinHtunOo <tinhtunhe@gmail.com>
2025-09-03 16:54:39 +0700

    --

--- app/_lib/prisma.js
+++ app/_lib/prisma.js
@@ -1,9 +1,9 @@
-const { PrismaClient } = require("@/src/generated/prisma");
-let prisma;
+import { PrismaClient } from "@prisma/client";
 
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
-}
-prisma = global.prisma;
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
